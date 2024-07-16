7f0810dc9c84fb8067821b03c8fd4528fd598bba
Sridhar <sridharas04@gmail.com>
2024-07-16 17:41:23 +0530

    Incremental Scan 1

--- config/db.js
+++ config/db.js
@@ -1,6 +1,15 @@
+// module.exports = {
+//   username: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   host: process.env.MYSQL_HOST || 'mysql-db',
+//   port: process.env.MYSQL_PORT || 3306,
+//   dialect: 'mysql'
+// }
+
 module.exports = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  username: "root",
+  password: "SupersecretPassword",
   database: process.env.MYSQL_DATABASE,
   host: process.env.MYSQL_HOST || 'mysql-db',
   port: process.env.MYSQL_PORT || 3306,
