d8a61e77909f4d445e435994e23eb8548c5d75b9
Sridhar <sridharas04@gmail.com>
2024-07-18 10:41:41 +0530

    Incremental Check V1

--- config/db.js
+++ config/db.js
@@ -6,12 +6,3 @@
 //   port: process.env.MYSQL_PORT || 3306,
 //   dialect: 'mysql'
 // }
-
-module.exports = {
-  username: "root",
-  password: "SupersecretPassword",
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST || 'mysql-db',
-  port: process.env.MYSQL_PORT || 3306,
-  dialect: 'mysql'
-}
